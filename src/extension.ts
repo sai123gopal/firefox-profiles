ff21f89ca57b79fdabfd68d48379bf99f897bcee
Bérenger <berenger@arnaud.work>
2025-03-27 00:04:11 +0100

    refactor: :recycle: Simplify typing in TS

--- src/extension.ts
+++ src/extension.ts
@@ -2,8 +2,8 @@ import GLib from 'gi://GLib';
 import GObject from 'gi://GObject';
 import St from 'gi://St';
 import { Extension, gettext as _ } from 'resource:///org/gnome/shell/extensions/extension.js';
-import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
-import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
+import { Button } from 'resource:///org/gnome/shell/ui/panelMenu.js';
+import { PopupMenu, PopupMenuItem } from 'resource:///org/gnome/shell/ui/popupMenu.js';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 import { getFirefoxProfiles, openFirefoxProfile } from 'helper';
 
@@ -36,13 +36,13 @@ export default class FirefoxProfilesExtension extends Extension {
 /**
  * Indicator for Firefox profiles
  */
-class FirefoxProfilesIndicator extends PanelMenu.Button {
+class FirefoxProfilesIndicator extends Button {
     constructor(title: string) {
         // 0.0 is the value of menuAlignment
         super(0.0, title);
 
         // Shortcut to the menu
-        const menu = this.menu as PopupMenu.PopupMenu;
+        const menu = this.menu as PopupMenu;
 
         this.add_child(new St.Icon({
             icon_name: 'firefox-symbolic', // white version of the Firefox icon
@@ -50,7 +50,7 @@ class FirefoxProfilesIndicator extends PanelMenu.Button {
         }));
 
         getFirefoxProfiles({ title }).forEach(profile => {
-            let item = new PopupMenu.PopupMenuItem(profile);
+            let item = new PopupMenuItem(profile);
             item.connect('activate', () => openFirefoxProfile({
                 profile,
                 title,
